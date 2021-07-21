cb062fec4973a8bbdc2db42d9ff579e54dcca208
Aidan Jalili <aidanjalili@gmail.com>
2021-07-21 10:02:19 -0400

    Made Retries on API Limit Error More Broad to Encompass All Errors
    This commit also now has updated data (partially) and includes high low prices in the data

--- RawDataFetcher/main.cpp
+++ RawDataFetcher/main.cpp
@@ -14,7 +14,7 @@ using namespace std;
 //TO DO:
 /*
  *
- * from 2018-12-31T09:30:00-04:00
+ * from 2018-01-01T09:30:00-04:00
  * to 2020-01-03T09:30:00-04:00
  */
 
@@ -26,12 +26,12 @@ void WriteToCsvOutputs(string filename, vector<alpaca::Bar>& InputBar)
     ofstream OutputFile(filename);
 
     //Write the columns
-    OutputFile << "Date, Open, Close, Volume\n";
+    OutputFile << "Date, Open, Close, High, Low, Volume\n";
 
     //Write the rows
     for(auto iter = InputBar.begin(); iter!=InputBar.end(); iter++)
     {
-        OutputFile << date::format("%F", chrono::sys_seconds{chrono::seconds( (*iter).time )} ) << "," << to_string((*iter).open_price) << "," << to_string((*iter).close_price) << "," << to_string((*iter).volume) << "\n";
+        OutputFile << date::format("%F", chrono::sys_seconds{chrono::seconds( (*iter).time )} ) << "," << to_string((*iter).open_price) << "," << to_string((*iter).close_price) << "," << to_string((*iter).high_price) << "," << to_string((*iter).low_price) << "," << to_string((*iter).volume) << "\n";
     }
 
     OutputFile.close();
@@ -72,22 +72,16 @@ int main()
     /*Loops thru tickers here*/ //--> should put into a seperate function later
     for (auto iter = assets.begin(); iter!=assets.end(); iter++)
     {
-        auto bars_response = client.getBars({(*iter).symbol}, "2018-12-30T09:30:00-04:00", "2020-01-03T09:30:00-04:00", "", "", "1D", 1000);
+        auto bars_response = client.getBars({(*iter).symbol}, "2018-01-01T09:30:00-04:00", "2020-01-03T09:30:00-04:00", "", "", "1D", 1000);
+
         if (auto status = bars_response.first; !status.ok())
         {
             std::cerr << "Error getting bars information: " << status.getMessage() << std::endl;
-            if (status.getCode() == 429)
-            {
-                iter--;
-                continue;
-            }
-            else
-            {
-                //in the future could log here in a txt file which tickers didn't work for whatever reason
-                continue;
-            }
-
+            //Just pray that this doesnt end up in a never ending loop --> originally designed to retry after rate limit has been reached
+            iter--;
+            continue;
         }
+
         auto bars = bars_response.second.bars[(*iter).symbol];
 
         WriteToCsvOutputs(EffectiveWorkingDirectory+"RawData/"+(*iter).symbol+".csv", bars);
