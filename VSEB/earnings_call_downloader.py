13d70bea8562e5cbdd0a0cbc66471b69a658ffa6
Aidan Jalili <aidanjalili@gmail.com>
2022-05-10 09:16:26 -0500

    Added python earnings call data fetcher

---
+++ VSEB/earnings_call_downloader.py
@@ -0,0 +1,23 @@
+import pandas as pd
+from datetime import datetime
+from datetime import timedelta
+import yahoo_fin.stock_info as si
+import dateutil.parser
+import csv
+
+
+# setting the report date
+report_date = datetime.now().date()
+timestampStr = report_date.strftime("%Y-%m-%d")
+
+
+todays_earnings = si.get_earnings_for_date(timestampStr)
+
+csv_columns = ['ticker','companyshortname','startdatetime','startdatetimetype','epsestimate','epsactual','epssurprisepct','timeZoneShortName','gmtOffsetMilliSeconds','quoteType']
+
+csv_file = "Earnings_Data.csv"
+with open(csv_file, 'w') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+    writer.writeheader()
+    for data in todays_earnings:
+        writer.writerow(data)
