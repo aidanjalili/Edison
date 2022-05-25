382d2c20c8cbc143a5508157a30164c099f7e00a
Aidan Jalili <aidanjalili@gmail.com>
2022-05-24 21:19:45 -0500

    nvm to last commit

--- VSEB/run.bash
+++ VSEB/run.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        bazel run --action_env CC=/usr/bin/clang-12 //:Edison --cxxopt='-std=c++20' --jobs=1
+        bazel run --action_env CC=/usr/bin/clang-12 //:Edison --cxxopt='-std=c++20'
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-        bazel run //:Edison --cxxopt='-std=c++20' --cpu=darwin_arm64 --jobs=1
+        bazel run //:Edison --cxxopt='-std=c++20' --cpu=darwin_arm64
 else
 	echo "UNKNOWN OS... EXITING..."
 fi
