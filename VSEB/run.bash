e545a812969039d810f0ab5a43ae76dd322fa569
Aidan Jalili <aidanjalili@gmail.com>
2022-05-24 21:12:32 -0500

    fixed bazel build ram prob

--- VSEB/run.bash
+++ VSEB/run.bash
@@ -1,8 +1,8 @@
 #!/bin/bash
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-        bazel run --action_env CC=/usr/bin/clang-12 //:Edison --cxxopt='-std=c++20'
+        bazel run --action_env CC=/usr/bin/clang-12 //:Edison --cxxopt='-std=c++20' --jobs=1
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-        bazel run //:Edison --cxxopt='-std=c++20' --cpu=darwin_arm64
+        bazel run //:Edison --cxxopt='-std=c++20' --cpu=darwin_arm64 --jobs=1
 else
 	echo "UNKNOWN OS... EXITING..."
 fi
