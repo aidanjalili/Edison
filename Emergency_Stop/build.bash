c1f82721745ec803d43df8f33ef2556062a61c0d
Aidan Jalili <aidanjalili@gmail.com>
2022-05-25 10:47:09 -0500

    updated emerg. stop. build.bash

--- Emergency_Stop/build.bash
+++ Emergency_Stop/build.bash
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         bazel build --action_env CC=/usr/bin/clang-12 //:Emergency_Stop --cxxopt='-std=c++20'
-        chmod a+x /home/aidanjalili03/Edison-Live/Edison/Emergency_Stop/bazel-bin/Emergency_Stop
+        chmod 777 /home/aidanjalili03/Edison-Live/Edison/Emergency_Stop/bazel-bin/Emergency_Stop
 elif [[ "$OSTYPE" == "darwin"* ]]; then
         bazel build //:Emergency_Stop --cxxopt='-std=c++20' --cpu=darwin_arm64
-        chmod a+x /Users/aidanjalili03/Desktop/Edison/Emergency_Stop/bazel-bin/Emergency_Stop
+        chmod 777 /Users/aidanjalili03/Desktop/Edison/Emergency_Stop/bazel-bin/Emergency_Stop
 else
 	echo "UNKNOWN OS... EXITING..."
 fi
